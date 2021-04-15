4b49fb6c5807437b192cd5668ad0bdcd69ef1445
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2021-04-16 04:24:06 +0500

    expense-tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <div className="ET">Expense Tracker</div>
+        <div className="YB">YOUR BALANCE:
+        <br/>
+        <span>$</span>
+        <span>260</span>
+        </div>
+        <br/>
+        <div className="in-ex">
+          <div className="In">Income</div>
+          <div className="Ex">Expense</div>
+        </div>
+      </div>
     </div>
   );
 }
