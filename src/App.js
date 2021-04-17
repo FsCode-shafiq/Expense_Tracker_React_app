6ea2859da86e824ac452cd0b737e33ded6e7ad36
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2021-04-18 01:55:28 +0500

    update

--- src/App.js
+++ src/App.js
@@ -12,8 +12,11 @@ function App() {
         </div>
         <br/>
         <div className="in-ex">
-          <div className="In">Income</div>
-          <div className="Ex">Expense</div>
+          <div className="In">Income:
+          <span className='in-am'> $ 500</span>
+          </div>
+          <div className="Ex">Expense:
+          <span className='ex-am'> $ 500</span></div>
         </div>
       </div>
     </div>
