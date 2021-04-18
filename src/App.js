fbeeab91b74da12dc06b0a5dbcab0222a29db9ca
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2021-04-19 04:48:44 +0500

    adding html and css

--- src/App.js
+++ src/App.js
@@ -5,18 +5,46 @@ function App() {
     <div className="App">
       <div className="main-container">
         <div className="ET">Expense Tracker</div>
-        <div className="YB">YOUR BALANCE:
-        <br/>
-        <span>$</span>
-        <span>260</span>
+        <div className="YB">
+          YOUR BALANCE:
+          <br />
+          <span>$</span>
+          <span>260</span>
         </div>
-        <br/>
+        <br />
         <div className="in-ex">
-          <div className="In">Income:
-          <span className='in-am'> $ 500</span>
+          <div className="In">
+            Income:
+            <span className="in-am"> $ 500</span>
           </div>
-          <div className="Ex">Expense:
-          <span className='ex-am'> $ 500</span></div>
+          <div className="Ex">
+            Expense:
+            <span className="ex-am"> $ 500</span>
+          </div>
+        </div>
+        <h3 className="his">History</h3>
+        <div className="his-lies">
+          <li>
+            <span className="discription">Cash</span>
+            <span className="amount">$ 600</span>
+          </li>
+          <li>
+            <span className="discription">Cash</span>
+            <span className="amount">$ 600</span>
+          </li>
+        </div>
+        <div className="New-add">Add new transaction</div>
+        <div className='form-div'>
+          <form>
+            <label>Enter Description</label>
+            <br />
+            <input type="text"></input>
+            <br />
+            <label>Enter Amount</label>
+            <br />
+            <input type="text"></input><br />
+            <button type='submit'>Submit</button>
+          </form>
         </div>
       </div>
     </div>
